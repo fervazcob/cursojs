b421ff6fc1e5881ae2dbe602829695ac68b2e6ee
Fernando Vazquez <fdovazqc@gmail.com>
2020-08-31 09:42:05 -0500

    Se agrego la funcionalidad de mover el circulo

--- js/dom-ejercicios/keyboard.js
+++ js/dom-ejercicios/keyboard.js
@@ -11,31 +11,32 @@ export function keyboard(canvas, character) {
       characterW = getComputedStyle($character).width,
       characterH = getComputedStyle($character).height;
 
-    function move(orientation) {
-      switch (orientation) {
-        case "vertical":
-          positionV = positionV.slice(0, positionV.indexOf("p"));
-          positionV = parseInt(positionV);
-          characterH = characterH.slice(0, characterH.indexOf("p"));
-          characterH = parseInt(characterH);
-          canvasH = canvasH.slice(0, canvasH.indexOf("p"));
-          canvasH = parseInt(canvasH);
-          break;
-        case "horizontal":
-          positionH = positionH.slice(0, positionH.indexOf("p"));
-          positionH = parseInt(positionH);
-          characterW = characterW.slice(0, characterW.indexOf("p"));
-          characterW = parseInt(characterW);
-          canvasW = canvasW.slice(0, canvasW.indexOf("p"));
-          canvasW = parseInt(canvasW);
-          break;
-        default:
-          break;
+    function move(x, y) {
+      positionH = positionH.slice(0, positionH.indexOf("p"));
+      positionH = parseInt(positionH);
+      positionV = positionV.slice(0, positionV.indexOf("p"));
+      positionV = parseInt(positionV);
+      characterW = characterW.slice(0, characterW.indexOf("p"));
+      characterW = parseInt(characterW);
+      characterH = characterH.slice(0, characterH.indexOf("p"));
+      characterH = parseInt(characterH);
+      canvasW = canvasW.slice(0, canvasW.indexOf("p"));
+      canvasW = parseInt(canvasW);
+      canvasH = canvasH.slice(0, canvasH.indexOf("p"));
+      canvasH = parseInt(canvasH);
+
+      let newPosH = positionH + x,
+        newPosV = positionV + y;
+
+      if (newPosH >= 0 && newPosH <= (canvasW - characterW)) {
+        $character.style.setProperty("left", `${newPosH}px`);
+      }
+
+      if (newPosV >= 0 && newPosV <= (canvasH - characterH)) {
+        $character.style.setProperty("top", `${newPosV}px`);
       }
 
     }
-
-
 
     if (e.altKey && e.key === "a") {
       alert("Has presionado las teclas de la alerta");
@@ -49,51 +50,25 @@ export function keyboard(canvas, character) {
 
     if (e.key === "ArrowUp") {
       e.preventDefault();
-      positionV = positionV.slice(0, positionV.indexOf("p"));
-      positionV = parseInt(positionV);
-      if (positionV >= 10) {
-        $character.style.setProperty("top", `${positionV - 10}px`);
-      }
+      move(0, -10);
       console.log("Has presionado la tecla Arriba");
     }
 
     if (e.key === "ArrowDown") {
       e.preventDefault();
-      positionV = positionV.slice(0, positionV.indexOf("p"));
-      positionV = parseInt(positionV);
-      characterH = characterH.slice(0, characterH.indexOf("p"));
-      characterH = parseInt(characterH);
-      canvasH = canvasH.slice(0, canvasH.indexOf("p"));
-      canvasH = parseInt(canvasH);
-
-      if ((positionV + characterH) <= (canvasH - 10)) {
-        $character.style.setProperty("top", `${positionV + 10}px`);
-      }
+      move(0, 10);
       console.log("Has presionado la tecla Abajo");
     }
 
     if (e.key === "ArrowLeft") {
       e.preventDefault();
-      positionH = positionH.slice(0, positionH.indexOf("p"));
-      positionH = parseInt(positionH);
-      if (positionH >= 10) {
-        $character.style.setProperty("left", `${positionH - 10}px`);
-      }
+      move(-10, 0);
       console.log("Has presionado la tecla Izquierda");
     }
 
     if (e.key === "ArrowRight") {
       e.preventDefault();
-      positionH = positionH.slice(0, positionH.indexOf("p"));
-      positionH = parseInt(positionH);
-      characterW = characterW.slice(0, characterW.indexOf("p"));
-      characterW = parseInt(characterW);
-      canvasW = canvasW.slice(0, canvasW.indexOf("p"));
-      canvasW = parseInt(canvasW);
-
-      if ((positionH + characterW) <= (canvasW - 10)) {
-        $character.style.setProperty("left", `${positionH + 10}px`);
-      }
+      move(10, 0);
       console.log("Has presionado la tecla Derecha");
     }
   });
